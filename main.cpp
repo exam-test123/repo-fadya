a6c637d670062dcfe2553a25c07ab739dbc0b84b
Abdelrahman ElWarraky <bodyelwarraky@gmail.com>
2023-12-18 20:51:55 +0200

    dev updates

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int sum(int a, int b);
+int sum(int a, int b, int c);
 
 int main()
 {
 	cout << "hello student!\n";
 	cout << "let's try new concepts";
 	cout << "Learning git is fun";
+	cout << sum(2, 4, 6);
 }
 
-int sum(int a, int b)
+int sum(int a, int b, int c)
 {
-	return a + b;
+	return a + b + c;
 }
 
