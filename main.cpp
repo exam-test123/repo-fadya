d92542280421516897f5f436f66b207eaaf32b6f
Abdelrahman ElWarraky <bodyelwarraky@gmail.com>
2023-12-18 20:54:25 +0200

    any

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int sum(int a, int b);
+int my_sum(int a, int b);
 
 int main()
 {
+	cout << "this line won't make a conflict\n";
+	cout << "Can you guess why?\n";
 	cout << "hello student!\n";
 	cout << "let's try new concepts";
 	cout << "Learning git is fun";
+	cout << sum(2, 4);
 }
 
-int sum(int a, int b)
+int my_sum(int a, int b)
 {
 	return a + b;
 }
